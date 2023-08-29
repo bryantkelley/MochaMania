cb4ac841377046e5c4efb1228ab09751d590c879
Bryant Kelley <bryant@bryantkelley.com>
2023-08-29 09:27:44 -0700

    Add delete button and delete function

--- screens/EditRating.tsx
+++ screens/EditRating.tsx
@@ -5,6 +5,9 @@ import {
   Button,
   KeyboardAvoidingView,
   ScrollView,
+  Modal,
+  View,
+  Text,
 } from "react-native";
 import { useContext, useEffect, useState } from "react";
 import { RatingsContext } from "../utils/Ratings";
@@ -12,13 +15,15 @@ import { StackParamList, MochaRating } from "../utils/types";
 import { MochaRatingInput } from "../components/MochaRatingInput";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RowView } from "../components/RowView";
 
 export const EditRating = ({ route }) => {
   const { rating } = route.params;
   const navigation = useNavigation<NativeStackNavigationProp<StackParamList>>();
-  const { updateRating } = useContext(RatingsContext);
+  const { updateRating, deleteRating } = useContext(RatingsContext);
 
   const [ratingToEdit, setRatingtoEdit] = useState<MochaRating>(rating);
+  const [deleteModalVisible, setDeleteModalVisible] = useState(false);
 
   useEffect(() => {
     navigation.setOptions({
@@ -39,6 +44,30 @@ export const EditRating = ({ route }) => {
     <KeyboardAvoidingView style={styles.container}>
       <ScrollView>
         <MochaRatingInput rating={ratingToEdit} setRating={setRatingtoEdit} />
+        <RowView style={styles.spaceEvenlyRow}>
+          <Button
+            title="Delete Rating"
+            onPress={() => setDeleteModalVisible(true)}
+            color={styles.dangerousButton.color}
+          />
+        </RowView>
+        <Modal visible={deleteModalVisible} presentationStyle="pageSheet" animationType="slide">
+          <View style={styles.modalContainer}>
+            <Text style={styles.modalText}>Delete this rating? This action is not reversible.</Text>
+            <RowView style={styles.spaceEvenlyRow}>
+              <Button
+                title="Confirm"
+                onPress={() => {
+                  deleteRating(rating.id);
+                  setDeleteModalVisible(false);
+                  navigation.navigate("MainView");
+                }}
+                color={styles.dangerousButton.color}
+              />
+              <Button title="Cancel" onPress={() => setDeleteModalVisible(false)} />
+            </RowView>
+          </View>
+        </Modal>
       </ScrollView>
     </KeyboardAvoidingView>
   );
@@ -55,40 +84,26 @@ const styles = StyleSheet.create({
     alignItems: "stretch",
     justifyContent: "flex-start",
   },
-  inputContainer: {
-    marginHorizontal: 8,
-    marginVertical: 8,
-  },
-  inputLabel: {
-    marginHorizontal: 8,
-    marginVertical: 4,
-  },
-  input: {
-    minHeight: 48,
-    padding: 8,
-    ...Platform.select({
-      ios: {
-        backgroundColor: PlatformColor("tertiarySystemBackground"),
-      },
-    }),
-    borderRadius: 16,
-  },
-  pickerInput: {
-    minHeight: 48,
-    padding: 4,
-    ...Platform.select({
-      ios: {
-        backgroundColor: PlatformColor("tertiarySystemBackground"),
-      },
-    }),
-    borderRadius: 16,
-  },
-  pickerInputContainer: {
-    flex: 1,
-    marginHorizontal: 4,
-  },
   spaceBetweenRow: {
     justifyContent: "space-evenly",
     marginHorizontal: 4,
+  },
+  modalContainer: {
+    padding: 16,
+  },
+  modalText: {
+    fontSize: 48,
+  },
+  spaceEvenlyRow: {
+    justifyContent: "space-evenly",
+    marginVertical: 16,
+  },
+
+  dangerousButton: {
+    ...Platform.select({
+      ios: {
+        color: PlatformColor("systemRed"),
+      },
+    }),
   },
 });
